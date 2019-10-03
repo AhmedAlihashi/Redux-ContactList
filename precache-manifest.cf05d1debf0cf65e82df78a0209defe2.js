c6c0a0af0a167d839305fe18375b9ea17cb39380
PremePanther <shpadza@gmail.com>
2019-10-03 16:40:45 -0400

    Updates

---
+++ precache-manifest.cf05d1debf0cf65e82df78a0209defe2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc6c35abff59f8680b467fa659f329c2",
+    "url": "/Redux-ContactList/index.html"
+  },
+  {
+    "revision": "90020b4c1111972c7977",
+    "url": "/Redux-ContactList/static/js/2.505f8840.chunk.js"
+  },
+  {
+    "revision": "ae804d07c4bdc0202b61",
+    "url": "/Redux-ContactList/static/js/main.be2f375e.chunk.js"
+  },
+  {
+    "revision": "a3b166402398917a6eab",
+    "url": "/Redux-ContactList/static/js/runtime-main.6c42b3e1.js"
+  }
+]);
